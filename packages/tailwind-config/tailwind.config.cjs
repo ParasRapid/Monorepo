9e036bfbc65f194263db33c24c313d0f60f3cfe1
Paras <paras@rapidlinks.org.in>
2025-05-08 18:37:54 +0530

    add the tailwind

---
+++ packages/tailwind-config/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ff5500",
+      },
+    },
+  },
+  plugins: [],
+};
